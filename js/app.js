b6395354d540935b8de32e2bb67241f39dc4fcd7
Yamo406 <93231355+Yamo406@users.noreply.github.com.>
2024-11-02 21:53:38 +0200

    finish building the navbar and the active section portion

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
@@ -20,14 +20,18 @@
 
 /**
  * Define Global Variables
- * 
+ *
 */
+
+const sections = document.querySelectorAll("section");
+const navbarFragment = new DocumentFragment();
+const navbarlist = document.querySelector('#navbar__list');
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -35,14 +39,49 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
+
+// navbar style
+navbarlist.classList.add('navbar__menu');
+
+sections.forEach((section) => {
+    const element = document.createElement('li');
+    const link = document.createElement('a');
+    link.textContent = section.dataset.nav;
+    link.classList.add('menu__link');
+
+
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        section.scrollIntoView({ behavior: "smooth" });
+    });
+
+    element.appendChild(link);
+    navbarFragment.appendChild(element);
+
+});
 
 // build the nav
 
+navbarlist.appendChild(navbarFragment);
 
 // Add class 'active' to section when near top of viewport
 
+// how would you select the Active section?
+// If the section in the middle of the viewport is the current section
+// then highlight it.
+document.addEventListener("scroll", (event) => {
+    sections.forEach((section, index) => {
+        let rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+            // Remove active class from all sections
+            sections.forEach(sec => sec.classList.remove("activeSection"));
+            // Add active class to the current section
+            section.classList.add("activeSection");
+        }
+    });
+})
 
 // Scroll to anchor ID using scrollTO event
 
@@ -50,10 +89,10 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
